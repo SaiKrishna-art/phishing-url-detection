540fce690f371b030b37dfabc967165a5aabd137
Sai Krishna <saikrishna63309@gmail.com>
2026-01-03 18:47:51 +0530

    case sensitive import bug solved.

--- frontend/phishing_frontend/src/App.jsx
+++ frontend/phishing_frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/about";
+import About from "./pages/About";
 import ContactMe from "./pages/ContactMe";
 import "/src/App.css";
 
